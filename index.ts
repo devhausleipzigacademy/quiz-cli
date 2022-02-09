7518f3cf32f2ab588ec47686b6b2cd4133602bd1
Dan McAtee <danielmcatee@me.com>
2022-02-09 10:35:16 +0100

    add askQuestion functionality and print high score

--- index.ts
+++ index.ts
@@ -1,22 +1,39 @@
-import { readFileSync } from "fs";
+import { read, readFileSync } from "fs";
 import * as rs from "readline-sync";
 import { Question, Score } from "./types";
+import chalk from "chalk";
 
-// ask the user for a name
+console.log(chalk.green.bold("Welcome to the Quiz!!!\n"));
 const playerName: string = rs.question("What's your name?\n> ");
-console.log(playerName);
-// keep track our own score
+console.log(chalk.yellow(`Welcome ${playerName}\n`));
 
-// add some colors to the output
+let score = 0;
 
-// need some questions
 const file = readFileSync("./questions.json", { encoding: "utf-8" });
 const questions: Question[] = JSON.parse(file);
 
-// ask questions
-// check if the answer is correct
+for (let i = 0; i < questions.length; i++) {
+  const answer = rs.question(`${questions[i].question}\n> `);
+  // check if the answer is correct
+  if (answer === questions[i].answer) {
+    console.log(chalk.green("Awesome your answer is correct"));
+    score += 2;
+  } else {
+    console.log(chalk.red("Oh no that was not correct"));
+    console.log(chalk.yellow(`The correct answer is ${questions[i].answer}`));
+    score -= 1;
+  }
+  console.log(chalk.yellowBright(`Your score is ${score}`));
+  console.log(chalk.blue("-----------------------\n"));
+}
 
-// need the highscores
+console.log(`Your final score is ${score}\n`);
+
+const scoresFile = readFileSync("./scores.json", { encoding: "utf-8" });
+const scores: Score[] = JSON.parse(scoresFile);
+
+console.log(chalk.bold("High Score"));
+console.table(scores);
 // need to see if we beat the highscore
 
 // mechanism to repeat the game
