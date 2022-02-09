1eef9809f41bcd8586b86f9b4941349b7a2264b5
Dan McAtee <danielmcatee@me.com>
2022-02-09 11:59:27 +0100

    addScore functionality

--- index.ts
+++ index.ts
@@ -1,40 +1,62 @@
-import { readFileSync } from "fs";
 import * as rs from "readline-sync";
 import { Question, Score } from "./types";
 import chalk from "chalk";
 import { getQuestions } from "./questions";
-import { getScores } from "./scores";
+import { addScore, getScores } from "./scores";
 
 console.log(chalk.green.bold("Welcome to the Quiz!!!\n"));
 const playerName: string = rs.question("What's your name?\n> ");
 console.log(chalk.yellow(`Welcome ${playerName}\n`));
 
-let score = 0;
-
 const questions: Question[] = getQuestions();
+const scores: Score[] = getScores();
+let keepPlaying: boolean = true;
 
-for (let i = 0; i < questions.length; i++) {
-  const answer = rs.question(`${questions[i].question}\n> `);
-  // check if the answer is correct
-  if (answer === questions[i].answer) {
-    console.log(chalk.green("Awesome your answer is correct"));
-    score += 2;
-  } else {
-    console.log(chalk.red("Oh no that was not correct"));
-    console.log(chalk.yellow(`The correct answer is ${questions[i].answer}`));
-    score -= 1;
+function playGame(): void {
+  let score = 0;
+
+  for (let i = 0; i < questions.length; i++) {
+    const answer = rs.question(`${questions[i].question}\n> `);
+    if (answer.toLowerCase() === questions[i].answer.toLowerCase()) {
+      console.log(chalk.green("Awesome your answer is correct"));
+      score += 2;
+    } else {
+      console.log(chalk.red("Oh no that was not correct"));
+      console.log(chalk.yellow(`The correct answer is ${questions[i].answer}`));
+      score -= 1;
+    }
+    console.log(chalk.yellowBright(`Your score is ${score}`));
+    console.log(chalk.blue("-----------------------\n"));
   }
-  console.log(chalk.yellowBright(`Your score is ${score}`));
-  console.log(chalk.blue("-----------------------\n"));
+  console.log(`Your final score is ${score}\n`);
+
+  addScore(playerName, score);
+
+  console.log(chalk.bold("High Score"));
+  console.table(scores);
+
+  let maxScore = scores[0].points;
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i].points > maxScore) {
+      maxScore = scores[i].points;
+    }
+  }
+
+  if (score > maxScore) {
+    console.log(chalk.inverse.bold("\nCongrats, you beat the highscore\n"));
+  } else {
+    console.log(chalk.inverse.bold("\nBetter luck next time\n"));
+  }
+
+  const anotherGame = rs.question(
+    "Do you wanna play another game?\nType yes/no\n> "
+  );
+
+  if (anotherGame.toLowerCase() === "no") {
+    keepPlaying = false;
+  }
 }
 
-console.log(`Your final score is ${score}\n`);
-
-const scores = getScores();
-
-console.log(chalk.bold("High Score"));
-console.table(scores);
-// need to see if we beat the highscore
-
-// mechanism to repeat the game
-// ask the user if the game should be repeated
+while (keepPlaying) {
+  playGame();
+}
--- scores.ts
+++ scores.ts
@@ -1,7 +1,22 @@
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import { Score } from "./types";
 
 export function getScores(): Score[] {
   const scoresFile = readFileSync("./scores.json", { encoding: "utf-8" });
   return JSON.parse(scoresFile);
+}
+
+export function addScore(myName: string, myPoints: number) {
+  const scores = getScores();
+
+  const newScore: Score = {
+    username: myName,
+    points: myPoints,
+  };
+
+  const newScores = [...scores, newScore];
+
+  const newScoresString = JSON.stringify(newScores);
+
+  writeFileSync("./scores.json", newScoresString);
 }
